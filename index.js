3757b4b7ad7c3ed473f89d202214c06ebbfaedba
Filip Sokol <sok.filip@gmail.com>
2022-05-30 22:09:12 +0200

    add enemies

--- index.js
+++ index.js
@@ -43,16 +43,74 @@ class Projectile {
   }
 }
 
-const player = new Player(canvas.width / 2, canvas.height / 2, 30, "blue");
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
 
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
+const player = new Player(canvas.width / 2, canvas.height / 2, 30, "blue");
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemy() {
+  setInterval(() => {
+    const radius = Math.random() * (40 - 10) + 10;
+
+    let x;
+    let y;
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+
+    const color = "green";
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   player.draw();
+
   projectiles.forEach((projectile) => {
     projectile.update();
+  });
+
+  enemies.forEach((enemy) => {
+    enemy.update();
   });
 }
 
@@ -73,8 +131,10 @@ window.addEventListener("click", (event) => {
 });
 
 animate();
+spawnEnemy();
 
 // Dodać strzelanie CHECK
-// Dodać meteory
+// Dodać meteory CHECK
 // Dodać hit meteorów
+// Dodać hit playera
 // Dodać scoreboard
